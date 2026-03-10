286e0b13e83bbdf2bc171df3042f6ffdb6058a18
Thomas Kane <Kane@sonicachiever.com>
2026-03-10 15:36:26 +0100

    Initial Commit

---
+++ REFRETTests/REFRETTests.swift
@@ -0,0 +1,16 @@
+//
+//  REFRETTests.swift
+//  REFRETTests
+//
+//  Created by Thomas Kane on 3/10/26.
+//
+
+import Testing
+
+struct REFRETTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
